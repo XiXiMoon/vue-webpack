77f851af8269861c13ebaa1e60f3966d4756560d
lilong <654368939@qq.com>
2020-03-27 15:49:24 +0800

    Tips:小功能——devtool中的sourcemap设置 说明：开发环境用"cheap-module-eval-source-map"，生产环境用"cheap-module-source-map"，具体soucemap的区别及作用需总结

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -8,6 +8,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(baseConfig, {
 	mode: "development",
+	devtool: 'cheap-module-eval-source-map',
 	entry: {
 		main: './src/main.js'
 	},
