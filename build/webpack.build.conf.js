8cb9aad363e71f87cc02e2c64fcb446af03a9927
lilong <654368939@qq.com>
2020-04-13 11:20:44 +0800

    打包速度优化(使用ParallelUglifyPlugin开启多进程压缩JS文件) 说明：使用UglifyJS插件压缩JS代码时，需要先将代码解析成Object表示的AST（抽象语法树），再去应用各种规则去分析和处理AST，所以这个过程计算量大耗时较多。ParallelUglifyPlugin可以开启多个子进程，每个子进程使用UglifyJS压缩代码，可以并行执行，能显著缩短压缩时间

--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -3,8 +3,8 @@ const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.conf');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
-const UglifyWebpackPlugin = require('uglifyjs-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
 
 const preConfig = merge(baseConfig, {
 	
@@ -114,18 +114,34 @@ const preConfig = merge(baseConfig, {
 
 		new BundleAnalyzerPlugin({
 			analyzerMode: 'static'
+		}),
+
+		new ParallelUglifyPlugin({
+			uglifyJS:{
+				output: {
+					beautify: false,	//是否输出可读性较强的代码，即会保留空格和制表符，默认为输出，为了达到更好的压缩效果，可以设置为false
+					comments: false		//是否保留代码中的注释，默认为保留，为了达到更好的压缩效果，可以设置为false
+				},
+				compress: {
+					warnings: false,	//是否在UglifyJS删除没有用到的代码时输出警告信息，默认为输出，可以设置为false关闭这些作用
+					drop_console: true,	//是否删除代码中所有的console语句，默认为不删除，开启后，会删除所有的console语句
+					collapse_vars: false,
+					reduce_vars: false
+				}
+			},
+			test: /.js$/g,	//使用正则去匹配哪些文件需要被 ParallelUglifyPlugin 压缩
+			include: [], //使用正则去包含被 ParallelUglifyPlugin 压缩的文件，默认为 []
+			exclude: [],	//使用正则去不包含被 ParallelUglifyPlugin 压缩的文件，默认为 [].
+			cacheDir: '',	//缓存压缩后的结果，下次遇到一样的输入时直接从缓存中获取压缩后的结果并返回，cacheDir 用于配置缓存存放的目录路径。默认不会缓存，想开启缓存请设置一个目录路径。
+			workerCount: '',	//开启几个子进程去并发的执行压缩。默认是当前运行电脑的 CPU 核数减去1
+			sourceMap: false	//是否为压缩后的代码生成对应的Source Map, 默认不生成，开启后耗时会大大增加，一般不会将压缩后的代码的sourceMap发送给网站用户的浏览器。
 		})
 	],
 
 	optimization: {
 		minimizer: [
 			//压缩css
-			new OptimizeCssAssetsWebpackPlugin(),
-			
-			//压缩js
-			new UglifyWebpackPlugin({
-                parallel: 4	//使用多进程并行运行可提高构建速度。并发运行的默认数量：os.cpus().length - 1。
-			})
+			new OptimizeCssAssetsWebpackPlugin()
         ]
 	}
 })
