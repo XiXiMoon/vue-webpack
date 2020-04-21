48ee05e5cee8edad7552ab32e57b725fa49241f6
lilong <654368939@qq.com>
2020-04-21 14:08:30 +0800

    Vue项目(配置Vue-router路由懒加载)

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,74 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Login from "views/login"  //登录
-import Registe from "views/registe"   //注册
-import Index from "views/index" //首页
-import ArticleList from "views/func/articleList"    //文章管理
-import FeedbackList from "views/func/feedbackList"  //反馈管理
-import MemberList from "views/func/memberList"      //会员管理
-import PackageList from "views/func/packageList"    //套餐管理
-import PermissionList from "views/func/permissionList"  //权限管理
-import StaticbookList from "views/func/staticBookList"  //静态文案管理
-import UserList from "views/func/userList"  //用户管理
-import SystemLog from "views/log/systemLog"    //系统权限
-
 Vue.use(Router)
+
+const Login = (resolve) => {    //登录
+    import('views/login').then((module) => {
+        resolve(module)
+    })
+}
+
+const Registe = (resolve) => {    //注册
+    import('views/registe').then((module) => {
+        resolve(module)
+    })
+}
+
+const Index = (resolve) => {    //首页
+    import('views/Index').then((module) => {
+        resolve(module)
+    })
+}
+
+const ArticleList = (resolve) => {   //文章管理
+    import('views/func/articleList').then((module) => {
+        resolve(module)
+    })
+}
+
+const FeedbackList = (resolve) => {   //反馈管理
+    import('views/func/feedbackList').then((module) => {
+        resolve(module)
+    })
+}
+
+const MemberList = (resolve) => {   //会员管理
+    import('views/func/memberList').then((module) => {
+        resolve(module)
+    })
+}
+
+const PackageList = (resolve) => {   //套餐管理
+    import('views/func/packageList').then((module) => {
+        resolve(module)
+    })
+}
+
+const PermissionList = (resolve) => {   //权限管理
+    import('views/func/permissionList').then((module) => {
+        resolve(module)
+    })
+}
+
+const StaticbookList = (resolve) => {   //静态文案管理
+    import('views/func/staticBookList').then((module) => {
+        resolve(module)
+    })
+}
+
+const UserList = (resolve) => {   //用户管理
+    import('views/func/userList').then((module) => {
+        resolve(module)
+    })
+}
+
+const SystemLog = (resolve) => {   //系统权限
+    import('views/log/systemLog').then((module) => {
+        resolve(module)
+    })
+}
+
 
 const routes = [
     {
@@ -81,5 +136,12 @@ const routes = [
 const router = new Router({
     routes
 })
+
+//后续可打断点调试此文件，方便研究vue-router的源码
+// console.log(Login)
+// console.log(Router)
+// console.log(Vue)
+// console.log(routes)
+// console.log(router)
 
 export default router
